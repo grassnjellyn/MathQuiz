d4f3b61e7313590c83c16c831c37c402784c705d
grassnjellyn <techatika79@gmail.com>
2024-01-19 17:10:30 +0700

    Berhasil coba menampilkan data role dan user

--- dao/UserDaoImpl.php
+++ dao/UserDaoImpl.php
@@ -5,9 +5,9 @@ class UserDaoImpl
     public function fetchAllUser()
     {
         $link = ConnectionUtil::getMySQLConnection();
-        $query = 'SELECT r.id_role AS "role_id_role", r.nama_role AS "nama_role", id_user, username, password, email, FROM user u LEFT OUTER JOIN role r ON u.role_id_role = r.id_role';
+        $query = 'SELECT r.id_role AS "role_id_role", r.nama_role AS "nama_role", id_user, username, password, email FROM user u LEFT OUTER JOIN role r ON u.role_id_role = r.id_role';
         $stmt = $link->prepare($query);
-        $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Dosen');
+        $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'User');
         $stmt->execute();
         $link = null;
         return $stmt->fetchAll();
